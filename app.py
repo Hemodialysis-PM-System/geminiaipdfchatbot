660e763e892a4d0872094d45f7296a7bbf4c1670
Lam Tsz Wai Michelle <mtwlam2-c@my.cityu.edu.hk>
2026-03-10 21:51:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from google import genai
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
+from langchain_classic.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
