0d2a774fd2aee69822034c2b1881b0b2d95be793
Lam Tsz Wai Michelle <mtwlam2-c@my.cityu.edu.hk>
2026-03-10 21:50:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-import os
+import streamlit as st
 from pypdf import PdfReader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import streamlit as st
 from google import genai
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
